b7bc6e1d3305e03640985c90d6c93d8a059c8b54
NzakiCodes <ekisnzaki@gmail.com>
2019-09-17 19:21:07 +0100

    Still Working**

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from './components/Navbar';
 // import * as Image from './images';
 import Logo from './images/navlogo2.png'
+import Header from './components/Header';
 
 function App() {
     return (
         <div>
             <Navbar logo={Logo}/>
+            <Header/>
         </div>
     )
 }
