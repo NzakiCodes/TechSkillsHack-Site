a526630549cfeb67c81eaca71a0b13b302d112e8
NzakiCodes <ekisnzaki@gmail.com>
2019-10-23 06:12:12 +0100

    Added Build

---
+++ build/precache-manifest.d455eda28199b6792cddeb3a9886d5e7.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63dbad645a95e0f781fa3a8458bb026",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd909bcec448177ba410",
+    "url": "/static/css/main.8530fa91.chunk.css"
+  },
+  {
+    "revision": "6b2952df8620e5758c00",
+    "url": "/static/js/2.0403b3bc.chunk.js"
+  },
+  {
+    "revision": "dd909bcec448177ba410",
+    "url": "/static/js/main.907c7210.chunk.js"
+  },
+  {
+    "revision": "917905f828d7dccccc3e",
+    "url": "/static/js/runtime~main.2804c711.js"
+  },
+  {
+    "revision": "ce6f1487402d6dd23e8306f21627439f",
+    "url": "/static/media/4.ce6f1487.jpg"
+  },
+  {
+    "revision": "96ce2e5dae143902082bf917cff1bed0",
+    "url": "/static/media/5.96ce2e5d.jpg"
+  },
+  {
+    "revision": "3d08587bebcde33f79df77cec588a436",
+    "url": "/static/media/6.3d08587b.png"
+  },
+  {
+    "revision": "0ffba4db39c7ba1ce03f8d30d338e45f",
+    "url": "/static/media/7.0ffba4db.jpg"
+  },
+  {
+    "revision": "ad9bfc914b02a335aae95071ca2f8e02",
+    "url": "/static/media/790e8fc660cbf96eb7581ea14608f240.ad9bfc91.jpg"
+  },
+  {
+    "revision": "bcd5d8fd1efe6333541aaae6bf36f828",
+    "url": "/static/media/bg.bcd5d8fd.jpg"
+  },
+  {
+    "revision": "dce0bdcd72162a984c4c5999b136da30",
+    "url": "/static/media/bg1.dce0bdcd.jpg"
+  },
+  {
+    "revision": "967a2311b03a583f3b9a324145457556",
+    "url": "/static/media/bg2.967a2311.jpg"
+  },
+  {
+    "revision": "6717def7eafa58f6255a588e8b36dbf2",
+    "url": "/static/media/bg4.6717def7.jpg"
+  },
+  {
+    "revision": "1bb1d28371b499a74606526ce39a7fec",
+    "url": "/static/media/bg6.1bb1d283.jpg"
+  },
+  {
+    "revision": "3f620e9337e6ee1773908f67653d7e00",
+    "url": "/static/media/bg7.3f620e93.jpg"
+  },
+  {
+    "revision": "62939bc5974e478207c92234562c5de3",
+    "url": "/static/media/bg8.62939bc5.jpg"
+  },
+  {
+    "revision": "3165b14bbf3b64fca65829ecde6b800d",
+    "url": "/static/media/fa-brands-400.3165b14b.eot"
+  },
+  {
+    "revision": "457cb96b6191ed105c4fe8463957f70d",
+    "url": "/static/media/fa-brands-400.457cb96b.woff"
+  },
+  {
+    "revision": "f249e44df3044e7b0d665b550569ddf0",
+    "url": "/static/media/fa-brands-400.f249e44d.svg"
+  },
+  {
+    "revision": "f2e186cfab4787d4ef6f1bb192aa9a1b",
+    "url": "/static/media/fa-brands-400.f2e186cf.ttf"
+  },
+  {
+    "revision": "f861a57c52ef711cf807a3eec92c0e17",
+    "url": "/static/media/fa-brands-400.f861a57c.woff2"
+  },
+  {
+    "revision": "2cd8d991e82712f1a5c5de8ee869ca74",
+    "url": "/static/media/fa-regular-400.2cd8d991.ttf"
+  },
+  {
+    "revision": "2fad4ee0a47f41bc76e21c2baf613182",
+    "url": "/static/media/fa-regular-400.2fad4ee0.svg"
+  },
+  {
+    "revision": "7ab2cb050690f93994c46258d49af5b6",
+    "url": "/static/media/fa-regular-400.7ab2cb05.woff"
+  },
+  {
+    "revision": "a03df7ab0ffc96cabbfe3dc71c60baaa",
+    "url": "/static/media/fa-regular-400.a03df7ab.eot"
+  },
+  {
+    "revision": "bd52a727b5449dc3f8195b72c9c58341",
+    "url": "/static/media/fa-regular-400.bd52a727.woff2"
+  },
+  {
+    "revision": "2cd2be177470d5096992572176bbe76e",
+    "url": "/static/media/fa-solid-900.2cd2be17.woff2"
+  },
+  {
+    "revision": "5ee32f5c8598e1a63ddf1aad4ffe54f4",
+    "url": "/static/media/fa-solid-900.5ee32f5c.ttf"
+  },
+  {
+    "revision": "657a469469a2fc38a2901c68a3d56512",
+    "url": "/static/media/fa-solid-900.657a4694.woff"
+  },
+  {
+    "revision": "a547e21eceadf53602caf057be9ad9fd",
+    "url": "/static/media/fa-solid-900.a547e21e.eot"
+  },
+  {
+    "revision": "fdc155d57b7351ef85b3028ea3cfc048",
+    "url": "/static/media/fa-solid-900.fdc155d5.svg"
+  },
+  {
+    "revision": "d7e60f9d1433a45ed71817f6d23abeca",
+    "url": "/static/media/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.d7e60f9d.woff2"
+  },
+  {
+    "revision": "5dfef10aeac08051a14687f86065ab71",
+    "url": "/static/media/learn-2001847_1920.5dfef10a.jpg"
+  },
+  {
+    "revision": "c6a29da462babe13aa8dd6d0c4b2fea4",
+    "url": "/static/media/logo.c6a29da4.png"
+  },
+  {
+    "revision": "4a2ea748ead04d42fe81a6d0105b9cde",
+    "url": "/static/media/navlogo2.4a2ea748.png"
+  }
+]);
